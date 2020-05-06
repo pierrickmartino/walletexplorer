107e5604c906fa21e8c99130041fd15fc583e4a7
pierrick.martino <pierrick.martino@gmail.com>
2020-05-06 09:00:28 +0200

    add Settings page UI

--- lib/ui/screens/settings.dart
+++ lib/ui/screens/settings.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
-import '../../util/data.dart';
-import '../widgets/account.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class Settings extends StatefulWidget {
   @override
@@ -9,24 +7,148 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  final TextStyle whiteText = TextStyle(
+    color: Colors.white,
+  );
+  final TextStyle greyTExt = TextStyle(
+    color: Colors.grey.shade400,
+  );
+
+  TextStyle whiteBoldText = TextStyle(
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+
+  List<String> avatars = [
+    'https://firebasestorage.googleapis.com/v0/b/dl-flutter-ui-challenges.appspot.com/o/img%2F1.jpg?alt=media',
+    'https://firebasestorage.googleapis.com/v0/b/dl-flutter-ui-challenges.appspot.com/o/img%2F4.jpg?alt=media',
+    'https://firebasestorage.googleapis.com/v0/b/dl-flutter-ui-challenges.appspot.com/o/img%2F6.jpg?alt=media',
+    'https://firebasestorage.googleapis.com/v0/b/dl-flutter-ui-challenges.appspot.com/o/img%2F7.jpg?alt=media',
+    'https://firebasestorage.googleapis.com/v0/b/dl-flutter-ui-challenges.appspot.com/o/img%2Fdev_damodar.jpg?alt=media&token=aaf47b41-3485-4bab-bcb6-2e472b9afee6',
+    'https://firebasestorage.googleapis.com/v0/b/dl-flutter-ui-challenges.appspot.com/o/img%2Fdev_sudip.jpg?alt=media',
+    'https://firebasestorage.googleapis.com/v0/b/dl-flutter-ui-challenges.appspot.com/o/img%2Fdev_sid.png?alt=media',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        physics: NeverScrollableScrollPhysics(),
-        primary: false,
-        itemCount: assets.length,
-        itemBuilder: (BuildContext context, int index) {
-          Map asset = assets[index];
-
-          return AccountUI(
-            name: asset['name'],
-            icon: asset['icon'],
-            rate: asset['rate'],
-            currency: asset['currency'],
-            color: asset['color'],
-          );
-        },
+      //backgroundColor: Colors.black,
+      body: Theme(
+        data: Theme.of(context).copyWith(
+          brightness: Brightness.dark,
+          primaryColor: Colors.purple,
+        ),
+        child: DefaultTextStyle(
+          style: TextStyle(
+            color: Colors.white,
+          ),
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              children: <Widget>[
+                const SizedBox(height: 30.0),
+                Row(
+                  children: <Widget>[
+                    Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: CachedNetworkImageProvider(avatars[1]),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 10.0),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "Jane Doe",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                          Text(
+                            "Nepal",
+                            style: TextStyle(
+                              color: Colors.grey.shade400,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20.0),
+                ListTile(
+                  title: Text(
+                    "Languages",
+                    style: whiteBoldText,
+                  ),
+                  subtitle: Text(
+                    "English US",
+                    style: greyTExt,
+                  ),
+                  trailing: Icon(
+                    Icons.keyboard_arrow_right,
+                    color: Colors.grey.shade400,
+                  ),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: Text(
+                    "Profile Settings",
+                    style: whiteBoldText,
+                  ),
+                  subtitle: Text(
+                    "Jane Doe",
+                    style: greyTExt,
+                  ),
+                  trailing: Icon(
+                    Icons.keyboard_arrow_right,
+                    color: Colors.grey.shade400,
+                  ),
+                  onTap: () {},
+                ),
+                SwitchListTile(
+                  title: Text(
+                    "Email Notifications",
+                    style: whiteBoldText,
+                  ),
+                  subtitle: Text(
+                    "On",
+                    style: greyTExt,
+                  ),
+                  value: true,
+                  onChanged: (val) {},
+                ),
+                SwitchListTile(
+                  title: Text(
+                    "Push Notifications",
+                    style: whiteBoldText,
+                  ),
+                  subtitle: Text(
+                    "Off",
+                    style: greyTExt,
+                  ),
+                  value: false,
+                  onChanged: (val) {},
+                ),
+                ListTile(
+                  title: Text(
+                    "Logout",
+                    style: whiteBoldText,
+                  ),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
